87390c443e22a3c7fbbbcd2b57941b8d9d6ed702
kurekish <nk.kurella@gmail.com>
2019-08-29 20:11:45 +0530

    control_flow

---
+++ control_flow.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func foo() {
+	fmt.Println("Iam in foo")
+}
+
+func main() {
+
+	fmt.Println("In main")
+	foo()
+
+	for i := 0; i <= 10; i++ { //loop
+		if i%2 == 0 { //conditional
+			fmt.Println(i)
+		}
+	}
+}
+
+
+
+//control flow
+//1. sequence
+//2. loop; iterative
+//3. conditional
