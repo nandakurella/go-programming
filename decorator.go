bc6b5c0b4ed95c8ca74a376a4351e2f77138126d
kurekish <nk.kurella@gmail.com>
2019-09-09 20:36:31 +0530

    operators,operands,identifiers,keywords

--- decorator.go
+++ decorator.go
@@ -2,7 +2,29 @@ package main
 
 import "fmt"
 
-func main(){
-x:=42
-fmt.Println(x)
+//To declare varibales outside function body. Scope is through out the file
+var z=35
+
+//if no explicit initialization is done, ZERO value of it type is assigned by default
+var a int
+var str string
+var b bool
+var c=5 // declared but not used.Its fine
+
+func main() {
+	//short declaration operator
+	//declaration and definition. any unused are treated as errors.
+	x := 42
+	fmt.Println(x)
+	//x variable datatype cant be changed
+	
+	fmt.Println(a) // Print 0
+	fmt.Println(b) // Prints false
+	fmt.Println(str) // Prints empty string
+	
+	foo()
+}
+
+func foo(){
+fmt.Println(z)
 }
